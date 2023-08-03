973ee82f769c60df442176fa07ef899f8b0e5946
saikumarchittaluri <127985962+saikumarchittaluri@users.noreply.github.com>
2023-08-03 22:22:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+// function volume_sphere() {}
+
 function volume_sphere() {
-    //Write your code here
-  
-} 
+  // Get the value of the radius input field
+  const radius = document.getElementById("radius").value;
+
+  // Check if the input value is a valid number
+  if (!isNaN(radius) && radius !== "") {
+    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+    // Set the calculated volume in the volume input field
+    document.getElementById("volume").value = volume.toFixed(2);
+  }
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
